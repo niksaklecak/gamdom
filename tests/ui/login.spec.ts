e9826f22faf53763c01cba1499d9ca026a5ea001
niksaklecakc060857 <148770018+niksaklecakc060857@users.noreply.github.com>
2025-05-08 13:39:00 +0200

    change

--- tests/ui/login.spec.ts
+++ tests/ui/login.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect, Page } from "@playwright/test";
-import { LoginPage } from "@pages/loginPage";
-import { config } from "@helpers/envHandler";
+import { LoginPage } from "../../pages/loginPage";
+import { config } from "../../helpers/envHandler";
+
 test.describe("Gamdom UI Tests @ui", () => {
   test("should login successfully", async ({ page }) => {
     const loginPage = new LoginPage(page);
